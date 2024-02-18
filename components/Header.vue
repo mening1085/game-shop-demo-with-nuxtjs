9ba691bc2fce09b0269fde6768807fe42625346d
Sungka <kasem.t@kisracorp.com>
2024-02-18 15:55:12 +0700

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="bg-gray-900 text-white">
+    <div class="container mx-auto flex justify-between items-center h-[50px]">
+      <div class="uppercase">
+        <nuxt-link to="/">Free to play</nuxt-link>
+      </div>
+      <ul class="flex gap-4">
+        <li>
+          <nuxt-link to="/">Home</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/about">About</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus dolorum vero
+    consectetur soluta saepe voluptatibus, architecto, temporibus quos nemo
+    cupiditate atque. Facere quisquam harum illum provident non sit libero
+    doloremque?
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
